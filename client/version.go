85a9a1ab99797cf165e0ee998fc4cadc29d6e0e9
Dorian Villet <gnutix@gmail.com>
2023-11-07 10:37:52 +0100

    Fix alfred version in local.

--- client/version.go
+++ client/version.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"math"
+
 	"github.com/gammadia/alfred/proto"
 	"github.com/spf13/cobra"
 )
@@ -10,12 +12,12 @@ var versionCmd = &cobra.Command{
 	Short: "Show the version number of Alfred",
 
 	RunE: func(cmd *cobra.Command, args []string) error {
-		cmd.Printf("alfred version %s (%s)\n", version, commit[:7])
+		cmd.Printf("alfred version %s (%s)\n", version, commit[:int(math.Min(float64(len(commit)), 7))])
 
 		if response, err := client.Ping(cmd.Context(), &proto.PingRequest{}); err != nil {
 			return err
 		} else {
-			cmd.Printf("server version %s (%s)\n", response.Version, response.Commit[:7])
+			cmd.Printf("server version %s (%s)\n", response.Version, response.Commit[:int(math.Min(float64(len(commit)), 7))])
 			return nil
 		}
 	},
